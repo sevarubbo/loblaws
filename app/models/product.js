6aaeda3a472afc04619c01c418fd155a533db9e0
Seva <seva.rubbo@gmail.com>
2017-03-02 11:43:44 -0500

    Update product.js

--- app/models/product.js
+++ app/models/product.js
@@ -9,7 +9,7 @@ export default DS.Model.extend({
     name: DS.attr("string"),
     price: DS.attr("number"),
     style: DS.attr("string"),
-    badge: DS.attr("badge"),
+    badge: DS.attr("string"),
     availableColors: DS.attr(),
     swatchImage: DS.attr("string"),
     thumbnails: DS.attr(),
